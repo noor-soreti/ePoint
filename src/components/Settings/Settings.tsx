0988e29db1dc51c1545f881bb5716ada8d6b4dc2
Noor Soreti <noor.said0698@gmail.com>
2024-12-08 20:25:16 +0300

    added MUI button

--- src/components/Settings/Settings.tsx
+++ src/components/Settings/Settings.tsx
@@ -1,12 +1,13 @@
-// import { useAuthenticator } from "@aws-amplify/ui-react"
-
+import { useAuthenticator } from "@aws-amplify/ui-react"
+import Button from '@mui/material/Button';
 
 export const Settings = () => {
-    // const { signOut } = useAuthenticator()
+    const { signOut } = useAuthenticator()
 
     return(
         <div>
             
+            <Button onClick={signOut} variant="contained" style={{backgroundColor: 'red'}}>Sign Out</Button>
         </div>
     )
 }
