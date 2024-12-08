8fd01d13487bf7d8832ee0a8049cb5442af8081e
Noor Soreti <noor.said0698@gmail.com>
2024-12-08 18:17:00 +0300

    test

--- src/App.tsx
+++ src/App.tsx
@@ -8,8 +8,8 @@ import { Outlet } from "react-router";
 const client = generateClient<Schema>();
 
 function App() {
-  const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
-  const { signOut } = useAuthenticator();
+  // const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
+  // const { signOut } = useAuthenticator();
 
   useEffect(() => {
     client.models.Todo.observeQuery().subscribe({
@@ -17,37 +17,18 @@ function App() {
     });
   }, []);
 
-  function createTodo() {
-    client.models.Todo.create({ content: window.prompt("Todo content") });
-  }
+  // function createTodo() {
+  //   client.models.Todo.create({ content: window.prompt("Todo content") });
+  // }
 
-  function deleteTodo(id: string) {
-    client.models.Todo.delete({ id })
-  }
+  // function deleteTodo(id: string) {
+  //   client.models.Todo.delete({ id })
+  // }
 
   return (
     <main>
-      {/* <h1>My todos</h1>
-      <button onClick={createTodo}>+ new</button>
-      <ul>
-        {todos.map((todo) => (
-          <li 
-          onClick={() => deleteTodo(todo.id)}
-          key={todo.id}>{todo.content}</li>
-        ))}
-      </ul>
-      <div>
-        🥳 App successfully hosted. Try creating a new todo.
-        <br />
-        <a href="https://docs.amplify.aws/react/start/quickstart/#make-frontend-updates">
-          Review next step of this tutorial.
-        </a>
-      </div>
-      <button onClick={signOut}>Sign out</button> */}
-
       <SideBar/>
       <Outlet/>
-
     </main>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -8,12 +8,12 @@ import { Authenticator, ThemeProvider } from '@aws-amplify/ui-react';
 import '@aws-amplify/ui-react/styles.css';
 import { BrowserRouter, Routes, Route } from "react-router";
 import { MainGrid } from "./components/MainGrid/MainGrid.tsx";
-// import { Settings } from "./components/Settings/Settings.tsx";
-// import { Analytics } from "./components/Analytics/Analytics.tsx";
-// import { Cards } from "./components/Cards/Cards.tsx";
+import { Settings } from "./components/Settings/Settings.tsx";
+import { Analytics } from "./components/Analytics/Analytics.tsx";
+import { Cards } from "./components/Cards/Cards.tsx";
 
 Amplify.configure(outputs);
-
+document.title = "TEST"
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <Authenticator >
@@ -22,9 +22,9 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
         <Routes>
           <Route path="/" element={<App />} >
             <Route index element={<MainGrid/>} />
-            {/* <Route path="cards/:pid" element={<Cards/>} />
+            <Route path="cards/:pid" element={<Cards/>} />
             <Route path="analytics" element={<Analytics/>} />
-            <Route path="settings" element={<Settings/>} /> */}
+            <Route path="settings" element={<Settings/>} />
           </Route>
         </Routes>
         </BrowserRouter>
