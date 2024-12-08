c23c26bae54aa03e36aac4205d43f8d4ed646009
Noor Soreti <noor.said0698@gmail.com>
2024-12-08 19:00:53 +0300

    removed unused comps

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,21 @@
-import { useEffect, useState } from "react";
+// import { useEffect, useState } from "react";
 import type { Schema } from "../amplify/data/resource";
 import { generateClient } from "aws-amplify/data";
-import { useAuthenticator } from '@aws-amplify/ui-react';
+// import { useAuthenticator } from '@aws-amplify/ui-react';
 import { SideBar } from "./components/SideBar/SideBar";
 import { Outlet } from "react-router";
 
-const client = generateClient<Schema>();
+// const client = generateClient<Schema>();
 
 function App() {
-  const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
+  // const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
   // const { signOut } = useAuthenticator();
 
-  useEffect(() => {
-    client.models.Todo.observeQuery().subscribe({
-      next: (data) => setTodos([...data.items]),
-    });
-  }, []);
+  // useEffect(() => {
+  //   client.models.Todo.observeQuery().subscribe({
+  //     next: (data) => setTodos([...data.items]),
+  //   });
+  // }, []);
 
   // function createTodo() {
   //   client.models.Todo.create({ content: window.prompt("Todo content") });
--- src/main.tsx
+++ src/main.tsx
@@ -21,10 +21,10 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
         <BrowserRouter>
         <Routes>
           <Route path="/" element={<App />} >
-            {/* <Route index element={<MainGrid/>} />
+            <Route index element={<MainGrid/>} />
             <Route path="cards/:pid" element={<Cards/>} />
             <Route path="analytics" element={<Analytics/>} />
-            <Route path="settings" element={<Settings/>} /> */}
+            <Route path="settings" element={<Settings/>} />
           </Route>
         </Routes>
         </BrowserRouter>
