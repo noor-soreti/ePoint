44b01cecbc67cd354129f51e677c1fc94d1796c8
Noor Soreti <noor.said0698@gmail.com>
2024-12-21 13:24:47 +0300

    added interface to tsconfig include

--- src/main.tsx
+++ src/main.tsx
@@ -4,20 +4,32 @@ import App from "./App.tsx";
 import { Amplify } from "aws-amplify";
 import outputs from "../amplify_outputs.json";
 import { Authenticator, ThemeProvider } from '@aws-amplify/ui-react';
-import '@aws-amplify/ui-react/styles.css';
 import { BrowserRouter, Routes, Route } from "react-router";
 import MainGrid from "./dashboard/components/MainGrid.tsx";
 import Cards from "./dashboard/components/Cards.tsx";
 import Analytics from "./dashboard/components/Analytics.tsx";
 import { Settings } from "./dashboard/components/Settings.tsx";
+import { generateClient } from "aws-amplify/api";
+import { Schema } from "../amplify/data/resource.ts";
+import '@aws-amplify/ui-react/styles.css';
+import { getCurrentUser } from 'aws-amplify/auth';
 
 Amplify.configure(outputs);
+
+// const client = generateClient<Schema>();
+// console.log(getCurrentUser());
+
+const currentUser = async () => {
+  console.log(await getCurrentUser())
+}
+
+currentUser()
+
 
 document.title = "ePoint"
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-
-      <Authenticator >
+      <Authenticator>
         <ThemeProvider>
           <BrowserRouter>
           <Routes>
